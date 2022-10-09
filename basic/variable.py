17ed05c69f83051c44cfaa6867a19dea7943aaf2
Antar Das <antardas2334@gmail.com>
2022-10-10 00:06:05 +0600

    initial commit

---
+++ basic/variable.py
@@ -0,0 +1,15 @@
+age = 15
+print(age)
+name = "Antar"
+print(name)
+age = 16
+print(age)
+# set variable constant
+PI = 3.14
+print(PI)
+# multiple variable
+a, b, c = 1, 2, 3
+print(a, b, c)
+
+# Make a variable constant
+PI = 3.14
