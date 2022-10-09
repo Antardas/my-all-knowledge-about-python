2ac8d4688d7efb3d56fec925b1e26fd664803827
Antar Das <antardas2334@gmail.com>
2022-10-10 00:33:32 +0600

    add type casting and take input

---
+++ basic/input.py
@@ -0,0 +1,12 @@
+
+
+# name = input("What is your name? ")
+# age = input("What is your age? ")
+# print(f'My name is {name} and my age is {age}')
+
+# Program: add two number
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+result = int(num1) + int(num2)
+
+print(f'The sum of {num1} and {num2} is {result}')
--- basic/variable.py
+++ basic/variable.py
@@ -13,3 +13,37 @@ print(a, b, c)
 
 # Make a variable constant
 PI = 3.14
+
+# Boolean variable
+is_python = True
+is_python = False
+print(is_python)
+
+# type check
+print(type(PI))
+print(type(is_python))
+
+# variable naming convention
+# 1. variable name should be meaningful
+# 2. variable name should be in lower case
+# 3. variable name should be in snake case
+# 4. variable name should not be in camel case
+# 5. variable name should not be in pascal case
+# 6. variable name should not be in kebab case
+# 7. variable name should not be in dot case
+# 8. variable name should not be in dash case
+# 9. variable name should not be in space case
+# 10. variable name should not be in title case
+# 11. variable name should not be in upper case
+
+# 1. can't start with number
+# 2. can't use special character
+# 3. can't use keyword
+# 4. can't use space
+# 5. can't use reserved word
+
+# F string
+name = "Antar"
+age = 15
+
+print(f'My name is {name} and my age is {age}')
